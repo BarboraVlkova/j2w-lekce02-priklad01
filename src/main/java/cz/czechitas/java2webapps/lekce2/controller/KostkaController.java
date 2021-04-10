1c87ada97eb8fb1423cd7ab36e64e7b7420e08cf
BarboraVlkova <bvlkova@email.cz>
2021-04-10 21:50:06 +0200

    Vytvoření generatoru nahodneho cisla na hod kostkou.

---
+++ src/main/java/cz/czechitas/java2webapps/lekce2/controller/KostkaController.java
@@ -0,0 +1,39 @@
+package cz.czechitas.java2webapps.lekce2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Random;
+
+
+/* @Controller rika, ze tahle mnou vytvorena trida je kontroler */
+/* rekne Stringu, vem tuhle tridu a chovej se k ni jako ke kontroleru */
+
+@Controller
+public class KostkaController {
+
+    /* metoda GET rika,ze chci ziskat tu stranku */
+    /* musi to byt trida a verejna */
+    /* @GetMapping("/") */
+
+
+    /* trida Random v balicku UtilRandom a generuje nahodna cisla*/
+    private final Random random;
+
+    public KostkaController() {
+        random = new Random();
+    }
+
+    /* metoda ModelAndView bude vracet tridu typu ModelAndView, ve kterym jsou informace MODEL a VIEW = data, ktera se maji zobrazit */
+    /* jaky data se maji zobrazit predam jako paramatr */
+    @GetMapping("/")
+    public ModelAndView hodKostkou() {
+        int nahodneCislo = random.nextInt(6)+1;
+
+        ModelAndView result = new ModelAndView("kostka");
+        result.addObject("cislo",nahodneCislo);
+        return result;
+
+    }
+}
