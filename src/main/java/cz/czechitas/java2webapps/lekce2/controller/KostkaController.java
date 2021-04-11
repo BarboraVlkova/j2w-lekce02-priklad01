7194b37d258fb7d531d3f532bbccdbc2d775b90e
BarboraVlkova <bvlkova@email.cz>
2021-04-11 09:00:44 +0200

    Final verze - ale nezobrazuje se v prohlizeci.

--- src/main/java/cz/czechitas/java2webapps/lekce2/controller/KostkaController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce2/controller/KostkaController.java
@@ -25,18 +25,20 @@ public class KostkaController {
         random = new Random();
     }
 
-    /* metoda ModelAndView bude vracet tridu typu ModelAndView, ve kterym jsou informace MODEL a VIEW = data, ktera se maji zobrazit */
-    /* jaky data se maji zobrazit predam jako paramatr */
+//    metoda ModelAndView bude vracet tridu typu ModelAndView, ve kterym jsou informace MODEL a VIEW = data, ktera se maji zobrazit
+//    jaky data se maji zobrazit predam jako paramatr
+
     @GetMapping("/")
     public ModelAndView hodKostkou() {
         int nahodneCislo = random.nextInt(6)+1;
-
         ModelAndView result = new ModelAndView("kostka");
         result.addObject("cislo",nahodneCislo);
-        /* ve tride string existuje metoda format */
-        /* jako prvni parametr da text (to je sablona) */
-        /* ma se vlozit cislo v desitkove soustave */
-        /* za carkou je hodnota, ktera se tam ma vlozit */
+
+//        ve tride string existuje metoda format
+//        jako prvni parametr da text (to je sablona)
+//        ma se vlozit cislo v desitkove soustave
+//        za carkou je hodnota, ktera se tam ma vlozit
+
         result.addObject("obrazek",String.format("/images/kostka-%d.svg", nahodneCislo));
         return result;
     }
